6b64379fc3259f3853d07d9d62acad3a7a2e3638
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-11-22 00:10:34 +0530

    hardcoded data removed

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -9,13 +9,13 @@ import { getMessaging, Messaging } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCr0aWM92orK80y0XCVhLsqRZFNiP67fcY",
-  authDomain: "task-management-77408.firebaseapp.com",
-  projectId: "task-management-77408",
-  storageBucket: "task-management-77408.appspot.com",
-  messagingSenderId: "159874062696",
-  appId: "1:159874062696:web:2e9133d942da759d7a5edd",
-  measurementId: "G-DLLKF3JHYT"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MESUREMENT_ID
 };
 
 // Initialize Firebase
